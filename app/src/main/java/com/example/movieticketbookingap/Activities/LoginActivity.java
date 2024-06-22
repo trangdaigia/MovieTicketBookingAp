59353b5e3425a20303144b0f6586612a4a820dda
trangdaigia <trangdaigia123@gmail.com>
2024-06-22 17:52:19 +0700

    dang ky dang nhap

--- app/src/main/java/com/example/movieticketbookingap/Activities/LoginActivity.java
+++ app/src/main/java/com/example/movieticketbookingap/Activities/LoginActivity.java
@@ -2,11 +2,18 @@ package com.example.movieticketbookingap.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,10 +21,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.movieticketbookingap.R;
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText username, password;
+    private EditText Email, Password;
     private TextView CreatAccount, forgetAcc;
     private Button loginBtn;
-   dbConnect DB;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,28 +35,39 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        username = findViewById(R.id.username1);
-        password = findViewById(R.id.password1);
+        Email = findViewById(R.id.email1);
+        Password = findViewById(R.id.password1);
         loginBtn = findViewById(R.id.loginBtn);
         CreatAccount = findViewById(R.id.TaoTaiKhoan);
         forgetAcc = findViewById(R.id.forget);
-        DB = new dbConnect(this);
+        mAuth = FirebaseAuth.getInstance();
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String user = username.getText().toString();
-                String pass = password.getText().toString();
+                String email = Email.getText().toString();
+                String password = Password.getText().toString();
 
-                if (user.equals("") || pass.equals("")) {
+                if (email.equals("") || password.equals("")) {
                     Toast.makeText(LoginActivity.this, "Hãy điền đầy đủ thông tin đăng nhập", Toast.LENGTH_SHORT).show();
                 } else {
-                    Boolean checkuserpass = DB.checkusernamepassword(user,pass);
-                    if (checkuserpass == true) {
-                        Toast.makeText(LoginActivity.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                    } else {
-                        Toast.makeText(LoginActivity.this, "Tài khoản và mật khẩu không đúng !", Toast.LENGTH_SHORT).show();
-                    }
+                    mAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Toast.makeText(LoginActivity.this, "Dang Nhap Thanh Cong.",
+                                                Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+
+                                        Toast.makeText(LoginActivity.this, "Dang Nhap That Bai.",
+                                                Toast.LENGTH_SHORT).show();
+
+                                    }
+                                }
+                            });
                 }
             }
         });
@@ -60,10 +79,4 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        forgetAcc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, ResetPassword.class));
-            }
-        });
     }}
--- app/src/main/java/com/example/movieticketbookingap/Activities/SignupActivity.java
+++ app/src/main/java/com/example/movieticketbookingap/Activities/SignupActivity.java
@@ -9,59 +9,60 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.movieticketbookingap.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
-    EditText fullname,Email,Phone,username,password,repassword;
+    EditText fullname,Email,Phone,username,Password,repassword;
     Button sigupBtn;
     dbConnect DB;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signup);
-        fullname = findViewById(R.id.fullname);
         Email = findViewById(R.id.Email);
-        Phone= findViewById(R.id.Phone);
-        username = findViewById(R.id.username);
-        password = findViewById(R.id.password);
+        Password = findViewById(R.id.password);
         repassword = findViewById(R.id.repassword);
         sigupBtn = findViewById(R.id.signupBtn);
+        mAuth = FirebaseAuth.getInstance();
 
 
         sigupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dbConnect DB = new dbConnect(SignupActivity.this);
-                String Hoten = fullname.getText().toString();
-                String Emailreg = Email.getText().toString();
-                String Sdt = Phone.getText().toString();
-                String user = username.getText().toString();
-                String pass = password.getText().toString();
+                String email = Email.getText().toString();
+                String password = Password.getText().toString();
                 String repass = repassword.getText().toString();
-                if (Hoten.equals("")||Emailreg.equals("")||Sdt.equals("")||user.equals("")||pass.equals("")||repassword.equals("")){
+                if (email.equals("")||password.equals("")||repassword.equals("")){
                     Toast.makeText(SignupActivity.this,"Hãy điền đầy đủ thông tin ", Toast.LENGTH_SHORT).show();
                 } else {
-                    if(pass.equals(repass)){
-                        Boolean checkuser = DB.checkusername(user);
-                        if(checkuser==false){
-                            Boolean insert = DB.insertData(Hoten,Emailreg,Sdt,user,pass);
-                            if ( insert == true){
-                                Toast.makeText(SignupActivity.this, "Đăng Ký Thành Công", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(SignupActivity.this,LoginActivity.class));
-                            }
-                            else {
-                                Toast.makeText(SignupActivity.this, "Đăng Ký Thất Bại", Toast.LENGTH_SHORT).show();
-                            }
-                        }else {
-                            Toast.makeText(SignupActivity.this, "Tài Khoản Đã Tồn Tại !", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                    else {
-                        Toast.makeText(SignupActivity.this, "Mật Khẩu Không Trùng Khớp", Toast.LENGTH_SHORT).show();
+                    if(password.equals(repass)){
+                        mAuth.createUserWithEmailAndPassword(email, password)
+                                .addOnCompleteListener(SignupActivity.this,new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            // Sign in success, update UI with the signed-in user's information
+                                            Toast.makeText(SignupActivity.this, "Dang Ky Thanh Cong.",
+                                                    Toast.LENGTH_SHORT).show();
+                                            startActivity(new Intent(SignupActivity.this,LoginActivity.class));
+                                        } else {
+                                            // If sign in fails, display a message to the user.
+                                            Toast.makeText(SignupActivity.this, "Dang Ky That Bai.",
+                                                    Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
                     }
                 }
             }
