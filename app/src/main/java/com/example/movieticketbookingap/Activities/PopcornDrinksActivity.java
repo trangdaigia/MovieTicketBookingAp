d655731cf510a218af918d8e418b04238efba97b
lethienhoaithuong <lethienhoaithuong.02@gmail.com>
2024-07-08 19:08:22 +0700

    push data bắp nưỡc

--- app/src/main/java/com/example/movieticketbookingap/Activities/PopcornDrinksActivity.java
+++ app/src/main/java/com/example/movieticketbookingap/Activities/PopcornDrinksActivity.java
@@ -9,18 +9,27 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.movieticketbookingap.R;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PopcornDrinksActivity extends AppCompatActivity {
 
     private TextView txtOriginalCount, txtCheeseCount, txtCaramelCount, txtCocaColaCount, txtSpriteCount, txtFantaCount;
     private Button btnOrder;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_popcorn_drinks);
+        
+        FirebaseApp.initializeApp(this);
+        databaseReference = FirebaseDatabase.getInstance().getReference("PopcornDrinkOrders");
 
-        // Initialize TextViews for counts
         txtOriginalCount = findViewById(R.id.txtOriginalCountValue);
         txtCheeseCount = findViewById(R.id.txtCheeseCountValue);
         txtCaramelCount = findViewById(R.id.txtCaramelCountValue);
@@ -28,7 +37,6 @@ public class PopcornDrinksActivity extends AppCompatActivity {
         txtSpriteCount = findViewById(R.id.txtSpriteCountValue);
         txtFantaCount = findViewById(R.id.txtFantaCountValue);
 
-        // Initialize Buttons for incrementing and decrementing
         setupIncrementDecrementButtons();
 
         btnOrder = findViewById(R.id.btnOrder);
@@ -64,13 +72,44 @@ public class PopcornDrinksActivity extends AppCompatActivity {
     }
 
     private void placeOrder() {
-        String message = "Bắp thường: " + txtOriginalCount.getText().toString() +
-                "\nBắp phô mai: " + txtCheeseCount.getText().toString() +
-                "\nBắp caramel: " + txtCaramelCount.getText().toString() +
-                "\nCoca Cola: " + txtCocaColaCount.getText().toString() +
-                "\nSprite: " + txtSpriteCount.getText().toString() +
-                "\nFanta Cam: " + txtFantaCount.getText().toString();
+        int originalCount = Integer.parseInt(txtOriginalCount.getText().toString());
+        int cheeseCount = Integer.parseInt(txtCheeseCount.getText().toString());
+        int caramelCount = Integer.parseInt(txtCaramelCount.getText().toString());
+        int cocaColaCount = Integer.parseInt(txtCocaColaCount.getText().toString());
+        int spriteCount = Integer.parseInt(txtSpriteCount.getText().toString());
+        int fantaCount = Integer.parseInt(txtFantaCount.getText().toString());
 
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        Map<String, Integer> items = new HashMap<>();
+
+        if (originalCount > 0) {
+            items.put("originalCount", originalCount);
+        }
+        if (cheeseCount > 0) {
+            items.put("cheeseCount", cheeseCount);
+        }
+        if (caramelCount > 0) {
+            items.put("caramelCount", caramelCount);
+        }
+        if (cocaColaCount > 0) {
+            items.put("cocaColaCount", cocaColaCount);
+        }
+        if (spriteCount > 0) {
+            items.put("spriteCount", spriteCount);
+        }
+        if (fantaCount > 0) {
+            items.put("fantaCount", fantaCount);
+        }
+
+        if (!items.isEmpty()) {
+            databaseReference.push().setValue(items).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    Toast.makeText(PopcornDrinksActivity.this, "Đã thêm bắp nước vào giỏ hàng", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(PopcornDrinksActivity.this, "Không thể thêm bắp nước vào giỏ hàng", Toast.LENGTH_LONG).show();
+                }
+            });
+        } else {
+            Toast.makeText(PopcornDrinksActivity.this, "Vui lòng chọn món", Toast.LENGTH_LONG).show();
+        }
     }
 }
